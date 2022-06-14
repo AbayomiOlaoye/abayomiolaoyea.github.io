4309418111c2e6d130c37994902337f886ab421e
Olaoye Oluwatoyin <mindprepopps@gmail.com>
2022-06-14 16:57:48 +0100

    Fixed some linter errors while waiting for partner's contibution

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 const menuButton = document.querySelector('.menu-btn');
 const nav = document.querySelector('.desktop-nav');
 const menuExit = document.querySelector('.close-nav');
-// //const padLinks = document.querySelectorAll('a.pad-link');
+const padLinks = document.querySelectorAll('a.pad-link');
 
-// menuButton.addEventListener('click', () => {
-//   nav.style.display = 'block';
-// });
+menuButton.addEventListener('click', () => {
+  nav.style.display = 'block';
+});
 
-// menuExit.addEventListener('click', () => {
-//   nav.style.display = 'none';
-// });
+menuExit.addEventListener('click', () => {
+  nav.style.display = 'none';
+});
 
-// padLinks.addEventListener('click', () => {
-//   const click = 1;
-//   for (let index = 0; index < padLinks.length; index += 1) {
-//     if (click === 1) {
-//       clickExit();
-//     }
-//   }
-// });
+padLinks.forEach((menu) => {
+  menu.addEventListener('click', () => {
+    nav.style.display = 'none';
+  });
+});
