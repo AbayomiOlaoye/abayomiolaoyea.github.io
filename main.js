0e001d9a253b679eeb9efb225ea1e970b01d86c8
Olaoye Oluwatoyin <mindprepopps@gmail.com>
2022-06-14 12:27:33 +0100

    Prep for Pair Programming jam on the Hamburger button

---
+++ main.js
@@ -0,0 +1,21 @@
+const menuButton = document.querySelector('.menu-btn');
+const nav = document.querySelector('.desktop-nav');
+const menuExit = document.querySelector('.close-nav');
+// //const padLinks = document.querySelectorAll('a.pad-link');
+
+// menuButton.addEventListener('click', () => {
+//   nav.style.display = 'block';
+// });
+
+// menuExit.addEventListener('click', () => {
+//   nav.style.display = 'none';
+// });
+
+// padLinks.addEventListener('click', () => {
+//   const click = 1;
+//   for (let index = 0; index < padLinks.length; index += 1) {
+//     if (click === 1) {
+//       clickExit();
+//     }
+//   }
+// });
